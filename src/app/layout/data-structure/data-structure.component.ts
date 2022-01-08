7215a87519d4f18b97c2a76f67cac6e1fd6f3a20
Rajesh Rathore <rajesh.rathore@stylework.city>
2022-01-08 13:26:53 +0530

    data structure hierarchy

--- src/app/layout/data-structure/data-structure.component.ts
+++ src/app/layout/data-structure/data-structure.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-structure.component.css']
 })
 export class DataStructureComponent implements OnInit {
+  
 
+  primitiveTypeShow = false;
+  nonprimitiveTypeShow = false;
+  linearData = false;
+  nonLinearData = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  primitiveDataTypeShow(): void {
+    this.primitiveTypeShow = true;
+    this.nonprimitiveTypeShow = false;
+  }
+
+  nonprimitiveDataTypeShow(): void {
+    this.primitiveTypeShow = false;
+    this.nonprimitiveTypeShow = true;
+  }
+
+  linearDataShow(): void {
+    this.nonLinearData = false;
+    this.linearData = true;
+  }
+
+  nonlinearDataShow(): void {
+    this.nonLinearData = true;
+    this.linearData = false;
   }
 
 }

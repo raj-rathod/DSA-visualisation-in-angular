5f3ef35d4c0706331b44d4afc9ee366793f77a79
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-20 05:30:50 +0530

    fix:<Divide and conquer> route name fix

--- src/app/components/Algorithms/divide-and-conquer/divide-and-conquer-routing.module.ts
+++ src/app/components/Algorithms/divide-and-conquer/divide-and-conquer-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     path: '', component: DivideAndConquerAlgorithmComponent
   },
   {
-    path: 'closet-pair-of-points', component: ClosestPairOfPointsComponent,
+    path: 'closest-pair-of-points', component: ClosestPairOfPointsComponent,
 
   },
   {

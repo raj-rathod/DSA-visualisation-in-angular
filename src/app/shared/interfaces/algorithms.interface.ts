1de07e05834e89ff73391e2f393106fbf140bdac
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-10-06 07:51:09 +0530

    feat:<Selection sort> selection sort meta data

---
+++ src/app/shared/interfaces/algorithms.interface.ts
@@ -0,0 +1,14 @@
+export interface Algorithms{
+    defination: string;
+    properties: string;
+    flowChart: string;
+    advantage: string;
+    disadvantage: string;
+    type?: string;
+    dataStructure?: string;
+    applications?: string;
+    memoryRepresentation?: string;
+    terminologies?: string;
+    other?: string;
+    workingProcedure?: string;
+}

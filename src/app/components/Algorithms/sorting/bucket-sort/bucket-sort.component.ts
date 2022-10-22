a1f9976807a213b1fb63a80802820b1bca47aa3e
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-10-22 21:08:25 +0530

    feat:<sidedrawer> sidedrawer for code highlight

--- src/app/components/Algorithms/sorting/bucket-sort/bucket-sort.component.ts
+++ src/app/components/Algorithms/sorting/bucket-sort/bucket-sort.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { bucketSortMetaData } from 'src/app/core/algorithms/sorting-meta-data';
 import { HighlightService } from 'src/app/shared/services/highlight-syntax.service';
@@ -5,16 +6,40 @@ import { HighlightService } from 'src/app/shared/services/highlight-syntax.servi
 @Component({
   selector: 'app-bucket-sort',
   templateUrl: './bucket-sort.component.html',
-  styleUrls: ['./bucket-sort.component.css']
+  styleUrls: ['./bucket-sort.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class BucketSortComponent implements OnInit {
   bucketSortMetaData = bucketSortMetaData;
+  menuState = 'out';
   constructor(
     private highlightSyntax: HighlightService
   ) { }
 
   ngOnInit(): void {
     this.highlightSyntax.highlightAll();
+  }
+
+  sideDrawer(): void{
+    if(this.menuState === 'out'){
+      this.menuState = 'in';
+      //document.body.style.overflow='hidden';
+    }else{
+      this.menuState = 'out';
+      //document.body.style.overflow='visible';
+    }
+   
   }
 
 }
---
+++ src/app/shared/components/code-syntax/code-syntax.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-syntax',
+  templateUrl: './code-syntax.component.html',
+  styleUrls: ['./code-syntax.component.css']
+})
+export class CodeSyntaxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

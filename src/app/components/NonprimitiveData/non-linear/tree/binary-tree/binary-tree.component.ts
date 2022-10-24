f88dd6dc9a105a7bac7a55083c66a1b8b94ee06e
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-10-24 18:46:03 +0530

    feat:<Binary Tree> serch result component set

--- src/app/components/NonprimitiveData/non-linear/tree/binary-tree/binary-tree.component.ts
+++ src/app/components/NonprimitiveData/non-linear/tree/binary-tree/binary-tree.component.ts
@@ -5,6 +5,7 @@ import { BinaryTree } from '@raj-rathod/dsa-methods';
 import { binaryTreeCodeData, binaryTreeMetaData } from 'src/app/core/data-structures/non-linear/tree/tree-meta-data';
 import { Helper } from 'src/app/helper/helper';
 import { singleInput, TreeInput } from 'src/app/helper/single-input-meta-data';
+import { SearchResultComponent } from 'src/app/shared/components/search-result/search-result.component';
 import { SingleValueInputDialogComponent } from 'src/app/shared/components/single-value-input-dialog/single-value-input-dialog.component';
 import { Operations, TraversalOperations } from 'src/app/shared/enums/basic.enum';
 import { CodeHighLight } from 'src/app/shared/interfaces/code-highlight.interface';
@@ -109,7 +110,18 @@ export class BinaryTreeComponent implements OnInit {
     matDialogRef.afterClosed().subscribe((result) => {
       if (result || result === 0) {
         this.binaryTree.searchNode(this.binaryTree.root, result);
-        console.log(this.binaryTree.findNode);
+        this.matDialog.open(SearchResultComponent, {
+          disableClose: true,
+          data:{ 
+            title:'Node Search In binary tree',
+            searchElement: result,
+            result:`${this.binaryTree.findNode !== null ? `
+            <p>Node <b>${this.binaryTree.findNode.data}</b> is present in binary tree</p>
+            `:`
+            <p>Node <b>${result}</b> is not present in the binary tree</p>
+            `}`
+          },
+        });
       }
     });
   }
@@ -150,16 +162,16 @@ export class BinaryTreeComponent implements OnInit {
     const nodes = this.binaryTree.totalNodesCount(this.binaryTree.root);
     const height = this.binaryTree.heightOfBinaryTree(this.binaryTree.root);
     return `<div class='mt-4 d-flex flex-wrap'>
-    <div class='data-section-active px-3 py-2 me-3 rounded'>
+    <div class='data-section-active px-3 py-2 me-3 mb-2 rounded'>
     <p class='m-0'>Height of binary tree: ${height}</p>
     </div>
-    <div class='data-section-active px-3 py-2 me-3 rounded'>
+    <div class='data-section-active px-3 py-2 me-3 mb-2 rounded'>
     <p class='m-0'>Levels of binary tree: ${height-1}</p>
     </div>
-    <div class='data-section-active px-3 py-2 me-3 rounded'>
+    <div class='data-section-active px-3 py-2 me-3 mb-2 rounded'>
     <p class='m-0'>Total number of nodes: ${nodes}</p>
     </div>
-    <div class='data-section-active px-3 py-2 me-3 rounded'>
+    <div class='data-section-active px-3 py-2 me-3 mb-2 rounded'>
     <p class='m-0'>Total number of edges: ${nodes-1}</p>
     </div>
     </div>`
